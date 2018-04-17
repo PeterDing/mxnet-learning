ef7152b696310e97846ccac5bf1f3a2e45f14fb2
PeterDing <dfhayst@gmail.com>
2018-04-17 12:59:33 +0800

    add dropout-gluon

---
+++ dropout-gluon/main.py
@@ -0,0 +1,56 @@
+from mxnet.gluon import nn
+from mxnet import nd
+from mxnet import autograd
+from mxnet import gluon
+import utils
+
+net = nn.Sequential()
+drop_prob1 = 0.2
+drop_prob2 = 0.5
+
+with net.name_scope():
+    net.add(nn.Flatten())
+    # 第一层全连接。
+    net.add(nn.Dense(256, activation="relu"))
+    # 在第一层全连接后添加丢弃层。
+    net.add(nn.Dropout(drop_prob1))
+    # 第二层全连接。
+    net.add(nn.Dense(256, activation="relu"))
+    # 在第二层全连接后添加丢弃层。
+    net.add(nn.Dropout(drop_prob2))
+    net.add(nn.Dense(10))
+net.initialize()
+
+batch_size = 256
+train_data, test_data = utils.load_data_fashion_mnist(batch_size)
+
+softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.5})
+
+
+def train():
+    for epoch in range(5):
+        train_loss = 0.
+        train_acc = 0.
+        for data, label in train_data:
+            with autograd.record():
+                output = net(data)
+                loss = softmax_cross_entropy(output, label)
+            loss.backward()
+            trainer.step(batch_size)
+
+            train_loss += nd.mean(loss).asscalar()
+            train_acc += utils.accuracy(output, label)
+
+        test_acc = utils.evaluate_accuracy(test_data, net)
+        print("Epoch %d. Loss: %f, Train acc %f, Test acc %f" %
+              (epoch, train_loss / len(train_data),
+               train_acc / len(train_data), test_acc))
+
+
+def main():
+    train()
+
+
+if __name__ == '__main__':
+    main()
