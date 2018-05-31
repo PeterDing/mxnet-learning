a90814e84be2e9510abd6328758f6f1fdb59b049
PeterDing <dfhayst@gmail.com>
2018-05-31 15:46:30 +0800

    [transformer/train.py] add ReduceLRScheduler for varying lr

--- transformer/train.py
+++ transformer/train.py
@@ -1,5 +1,7 @@
 from mxnet import nd
 from mxnet.gluon import loss as gloss
+from mxnet import lr_scheduler
+
 
 # smoothed cross_entropy
 # https://github.com/awslabs/sockeye/blob/master/sockeye/loss.py#L153
@@ -16,9 +18,26 @@ def get_loss(pred, label, num_classes, trg_pad, smooth_alpha=0.1):
     loss = (1 - smooth_alpha - pre_class_gain) * ce - nd.sum(
         pre_class_gain * logprob, axis=-1, keepdims=False)
 
-    mask = (label != trg_pad).astype(dtype=pred.dtype)
-    loss *= mask
+    mask = label != trg_pad
+    loss = loss * mask
 
     loss = nd.sum(loss) / mask.sum()
 
     return loss
+
+
+class ReduceLRScheduler(lr_scheduler.LRScheduler):
+
+    def __init__(self, model_dim, warmup_steps):
+        self.model_dim = model_dim
+        self.warmup_steps = warmup_steps
+        self._a = model_dim**-0.5
+        self._b = self.warmup_steps**-1.5
+
+    def __call__(self, num_update):
+        # lr = model_dim^(-0.5) * min(step_num^(-0.5), step_num * warmup_steps^(-1.5))
+        if num_update < self.warmup_steps:
+            lr = self._a * num_update * self._b
+        else:
+            lr = self._a * num_update**-0.5
+        return lr
